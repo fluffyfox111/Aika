63bab19281b83fa0da92249fb454ded1602e172b
NY <161125810+fluffyfox111@users.noreply.github.com>
2024-06-19 19:55:00 +0000

    Update player.js

--- player.js
+++ player.js
@@ -5,10 +5,10 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
    {
-    host: "lavalink.oryzen.xyz",
-    port: 80, 
-    password: "oryzen.xyz", 
-     secure: false
+    Host : lavalink.api.timelessnesses.me,
+    Port : 80,
+    Password : "youshallnotpass",
+    Secure : false
   },
     ];
 
