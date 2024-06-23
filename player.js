53b15e3c21567f78ccc689b601f563129199c99f
NY <161125810+fluffyfox111@users.noreply.github.com>
2024-06-23 15:48:26 +0000

    Update player.js

--- player.js
+++ player.js
@@ -5,9 +5,9 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
   {
-    host : "lava-v3.ajieblogs.eu.org",
+    host : "lavalink.api.timelessnesses.me",
     port : 80,
-    password : "https://dsc.gg/ajidevserver",
+    password : "youshallnotpass",
     secure : false
   },
     ];
